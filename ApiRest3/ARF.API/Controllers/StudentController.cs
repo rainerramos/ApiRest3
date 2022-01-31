2e34c2dcb68c44601c1e3bb787ae1275529e6de2
Rainer <rainermr89@hotmail.com>
2022-01-31 14:41:50 -0300

    Criando controller

---
+++ ApiRest3/ARF.API/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using ARF.Domain;
+using ARF.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ARF.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Add(Student student)
+        {
+            StudentService service = new StudentService();
+            service.AddStudent(student);
+
+
+            return StatusCode(201);
+        }
+    }
+}
