811a37ed74f97894ec8609cab3b3ac139117a7f3
Rainer <rainermr89@hotmail.com>
2022-01-31 14:26:30 -0300

    Criando domain(correção)

--- ApiRest3/ARF.Domain/Student.cs
+++ ApiRest3/ARF.Domain/Student.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ARF.Domain
 {
-    internal class Student
+    public class Student
     {
+        public Student()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+
+        [Required(AllowEmptyStrings =false, ErrorMessage = "Nome é obrigatório")]
+        [MinLength(3, ErrorMessage = "O nome deve ter mais que 2 caracteres")]
+        [MaxLength(150, ErrorMessage = "O nome deve ter 150 ou menos caracteres")]        
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Data de nascimento é obrigatório")]
+        public DateTime Birthday { get; set; }
+
+        [Required(ErrorMessage = "CPF é obrigatório")]
+        public string CPF { get; set; }
     }
 }
